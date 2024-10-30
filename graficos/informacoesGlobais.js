30ce7cb624a93a7d34ee322332ee492108a8f29a
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2024-10-30 06:57:16 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,9 @@
 const url = 'https://raw.githubusercontent.com/teuzsw/graficos-alura/refs/heads/main/dados-globais.json'
 
-console.log(url);
+async function visualizarInformacoes() {
+    const resposta = await fetch(url) - fetch faz requisicao
+    const dados = resposta.json()
+    console.log(dados);
+}
+
+visualizarInformacoes()
